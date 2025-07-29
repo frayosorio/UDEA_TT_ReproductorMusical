ba71d3a706733b9a77424d760beba105c87c0356
fray osorio <frayosorio@gmail.com>
2025-07-29 17:23:28 -0500

    Se agrega barra de desplazamiento para la reproduccion

--- codigo/app.js
+++ codigo/app.js
@@ -1,5 +1,5 @@
 angular.module("Reproductor", [])
-    .controller("ControladorReproductor", function ($scope, $http) {
+    .controller("ControladorReproductor", function ($scope, $http, $interval) {
 
         $scope.artistas = [];
         $scope.canciones = [];
@@ -7,6 +7,8 @@ angular.module("Reproductor", [])
         $scope.cancionSeleccionada = "";
         $scope.rutaCancion = "";
         $scope.reproductor = new Audio();
+        $scope.duracion = 0;
+        $scope.tiempoActual = 0;
 
         //cargue de los datos (solo posible mediante servicio)
         $http.get("datos/Musiteca.json")
@@ -23,8 +25,9 @@ angular.module("Reproductor", [])
             $scope.cancionSeleccionada = cancion.Titulo + " - " + $scope.artistaSeleccionado;
             $scope.rutaCancion = "canciones/" + $scope.artistaSeleccionado + " - " + cancion.Titulo + ".mp3";
             $scope.reproductor.src = $scope.rutaCancion;
+            $scope.duracion = $scope.reproductor.duration;
+            $scope.tiempoActual = 0;
         }
-
 
         $scope.reproducir = function () {
             $scope.reproductor.play();
@@ -33,4 +36,19 @@ angular.module("Reproductor", [])
         $scope.pausar = function () {
             $scope.reproductor.pause();
         }
+
+        $scope.ubicarTiempoCancion = function () {
+            $scope.reproductor.currentTime = $scope.tiempoActual;
+        }
+
+        $interval(function () {
+            if (!$scope.reproductor.paused) {
+
+                //window.alert($scope.reproductor.currentTime);
+
+
+                $scope.tiempoActual = $scope.reproductor.currentTime;
+            }
+        }, 500,0,true);
+
     });
